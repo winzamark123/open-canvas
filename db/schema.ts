13f051a9d58068b913b9e1c2f1a1bff21a7a8b6d
Win Cheng <winzamark12@gmail.com>
2025-10-26 14:42:14 -0700

    db schema and webhook set

--- db/schema.ts
+++ db/schema.ts
@@ -15,6 +15,8 @@ export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
   clerkId: varchar("clerk_id", { length: 255 }).notNull().unique(),
   email: varchar("email", { length: 255 }).notNull(),
+  firstName: varchar("first_name", { length: 255 }),
+  lastName: varchar("last_name", { length: 255 }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
