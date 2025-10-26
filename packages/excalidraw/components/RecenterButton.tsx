2e9a12afcdf85a8e1f61fe3e01e88034c1b5acc0
Win Cheng <winzamark12@gmail.com>
2025-10-26 09:25:07 -0700

    update recenter icon

--- packages/excalidraw/components/RecenterButton.tsx
+++ packages/excalidraw/components/RecenterButton.tsx
@@ -2,17 +2,9 @@ import clsx from "clsx";
 
 import { ToolButton } from "./ToolButton";
 import { createIcon } from "./icons";
+import { Minimize } from "lucide-react";
 
 import "./ToolIcon.scss";
-
-const recenterIcon = createIcon(
-  <g strokeWidth={1.25}>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
-  </g>,
-  24,
-);
 
 type RecenterButtonProps = {
   title?: string;
@@ -24,7 +16,7 @@ export const RecenterButton = (props: RecenterButtonProps) => {
     <ToolButton
       className={clsx("Shape")}
       type="button"
-      icon={recenterIcon}
+      icon={<Minimize />}
       title={props.title}
       aria-label={props.title || "Recenter canvas"}
       data-testid="toolbar-recenter"
